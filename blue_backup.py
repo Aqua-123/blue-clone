0af2b71c13cf6fe8bf3777d3b189014d9e9fd62f
Manasvi Kapoor <aqua@something.com>
2021-11-26 19:04:53 +0530

    new

--- blue_backup.py
+++ blue_backup.py
@@ -116,6 +116,7 @@ restart_s = re.compile(r"""((blue|blew) restart)|((blue|blew) reset)\s*""", re.I
 hide = re.compile(r"""blue help me hide\s*""", re.I)
 ily = re.compile(r"""blue (ily)|(i love you)\s*""", re.I)
 love = re.compile(r"""blue gift love\s*""", re.I)
+low_five = re.compile(r"""(blue )?(low five)\s*""", re.I)
 
 coffee = re.compile(r"""blue serve (coffee|1|caffee)\s*""", re.I)
 milk = re.compile(r"""blue serve (milk|2)\s*""", re.I)
@@ -168,6 +169,7 @@ howdy_r = "hewwos"
 no_r = "Kindly be nice and keep this family-friendly while you are here, else the wfaf door is always open for you to leave, thanks"
 dni_r = "We are not interested, thanks no thanks"
 ily_r = "I love you even moreeee"
+low_five_r = "Even lower five ~*"
 love_r = "Hey wonderful person, you are amazing and deserve everything you desire and love. Hope the best for you. You have all my love and wishes. Much love ~ Blue :>"
 response_dict = {
     tldr : tldr_r,
@@ -191,6 +193,7 @@ response_dict = {
     dni : dni_r,
     dying : dying_r,
     love : love_r,
+    low_five : low_five_r,
 
 
     coffee : coffee_r,
@@ -607,8 +610,15 @@ while running == True :
                                                 j= j+1
                                             responses = "Sending lotsa love and hugs to " + name+" ❤️❤️"
                                             send_message(responses)
-                                        
-    
+                            elif: (array[1]== "bonk"):
+								 new_arr = []
+									for i in range(2,len(array)):
+										new_arr.append(array[i])
+									name = ""
+									for j in range (0,len(new_arr)):
+										name = name + new_arr[j] + " "
+										j= j+1
+									response = "*bonks "+name  +" with a baseball bat~*"
                     j = b["user"]
                     if "id" in j.keys() :
                         id = j["id"]
