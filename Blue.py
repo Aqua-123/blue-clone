384fbcd86a6b8ed40e9e23a939f33b87a2b4be15
Aqua-123 <https://Aqua-123@github.com>
2022-01-23 02:18:33 +0530

    New changes to git commit schema

--- Blue.py
+++ Blue.py
@@ -312,11 +312,12 @@ def coin_handling(array):
             else: coin_confirm = str(int(num) + 0) + " coins added to the fortune well, there are now " + str(coin_new) + " coins in the well, wishing good luck to all :D"
         elif coin_add > 100: coin_confirm = "Woops too many coins, maybe buy me some chocolates instead? :>"
         send_message(coin_confirm)
-
+respons = ""
 def send_feelings(array,index,id):
     """Handles sending and recieving feelings 
     like hugs and love and what not I will be adding
     because yay feelings"""
+    global respons
     if index != 4:
         del array [0:4]
         name = fix_name(" ".join(array))
