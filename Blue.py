d1641cd190f8d882b12a8849e22806734475751e
Aqua-123 <https://Aqua-123@github.com>
2022-01-23 09:45:43 +0530

    a

--- Blue.py
+++ Blue.py
@@ -343,7 +343,7 @@ def admin_func(message,id,isadmin):
                     threading.Thread(target=stalker, args=(id,timer(),) )
                     stalker(id,timer())
                     send_message("Okai waking stalk function")
-                else: send_message("Please give a valid ID")
+                else: send_message(id)
                 
 def coin_handling(array):
     """Just as the name suggests,
